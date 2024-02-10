f9808765324384ffa0216c0b7c1b622090b8e916
Lindsay Drozdik <linds.drozdik@yahoo.com>
2024-02-10 11:05:17 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
+
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+cats;
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+}
+cats;
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+}
+cats;
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+cats;
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+cats;
+
+function appendCat(name) {
+    const cats = ["Milo", "Otis", "Garfield"];
+    const moreCats = [...cats, "Broom"];
+    return moreCats;
+}
+
+function prependCat(name) {
+    const moreCats = ["Arnold", ...cats];
+    return moreCats;
+}
+
+function removeLastCat() {
+    const updatedCats = cats.slice(0, -1);
+    return updatedCats;
+}
+
+function removeFirstCat() {
+    const updatedCats = cats.slice(1,3)
+    return updatedCats;
+}
